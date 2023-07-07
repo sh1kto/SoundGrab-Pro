e1e9260fa65e11dd6127fd2cdb4175f939b8c197
sh1kto <imshikto@gmail.com>
2023-07-08 01:55:02 +0600

    Gui Blank

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename,asksaveasfilename
+import moviepy.editor as me
+
+root=Tk()
+
+root.geometry("600x350")
+root.minsize(600,350)
+root.maxsize(600,350)
+root.title('SoundGrab Pro v0.0.1-alpha')
+
+root.mainloop()
